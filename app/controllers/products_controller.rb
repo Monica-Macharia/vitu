52a5cd2de488a72a57a5416cfda6c6254b2542ca
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-11 21:50:57 +0300

    fix: fix product model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,23 @@
 class ProductsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index 
+        render json: Product.all, status: :ok
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    private
+
+    def find_product
+        product = Product.find_by(id: params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "Product not found" }, status: :not_found
+    end
 end
