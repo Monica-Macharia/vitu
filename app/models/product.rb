37db3540c1c9541cbfb749072de6222b922d51a6
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-11 17:06:42 +0300

    feat: add relationships + validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+    has_many :purchases
+    has_many :users, through: :purchases
+
+    validates :category, presence: true
+    validates :description, length: 50, presence: true
+    validates :price, numericality: { greater_than: 0 }, presence: true
 end
