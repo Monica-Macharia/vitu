179cdb71bbf96ec03f66dfced9208c00c86bd917
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-10-11 21:51:51 +0300

    spacing

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,14 @@
 class UserController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+    
+    
     def show
         user = User.find_by!(id: [params: :id])
         render json: user, serializer: UserSerializer, status: :ok
     end
 
+    
+    
     private
 
     def not_found_response
