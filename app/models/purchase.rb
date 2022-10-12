5dd2a9d277e68e6c016040baf44e9851b028815e
davidnene <davienesh4@gmail.com>
2022-10-12 09:20:19 +0300

    Add associations and validations to purchase model

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,8 @@
 class Purchase < ApplicationRecord
+    belongs_to :product
+    belongs_to :user
+
+    validates :product_name, :quantity, :product_id, :user_id, presence: true
+    validates :quantity, :product_id, :user_id, numericality: { only_integer: true }
+
 end
