9b39fb62a5e81daf137e643b90914509dea93eb1
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-13 10:16:28 +0300

    update products component to fetch data from rails api

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
 
 
 const Products = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("/products")
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, [])
+
+
     return (
         <div>
-        <h1>Products</h1>
+        <h3>Our Products</h3>
+        <div className='product-container'>
+            {products.map(product =>(
+                <div key={product.id} className='product-item'>
+                    image: {product.image}
+                    name: {product.name}
+                    category: {product.category}
+                    description: {product.description}
+                    price: {product.price}
+                    review: {product.review}
+                </div>
+            ))}
+        </div>
         </div>
         );
 };
