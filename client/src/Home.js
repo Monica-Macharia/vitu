dd9fb02d17182407b9b7bd192a09f0b29c219e29
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-10-13 20:43:11 +0300

    cover page

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,11 +3,16 @@ import React from 'react';
 
 function Home(){
     return(
-        <div className= "intro">
-           <h2 className= "catch">VituVizuri is a one-stop-shop for creative pieces.</h2>
-            <p className= "peek">Displaying a collection of branded mugs, hoodies and T-shirts from 
-            <p></p>different artists across the country</p>
+        <div className= "phrase">
+            <div className= "intro">
+        
+           <h2 className= "catch">VituVizuri is a one-stop-shop <br/>for creative pieces.</h2>
+            <p className= "peek">Displaying a collection of branded mugs, hoodies<br/> and T-shirts from 
+            different artists across the country</p>
             <button className= "explore">DISCOVER NOW</button>
+             </div>
+            
+            <img className= "profile" src={window.location.origin + '/cover.jpg' }alt="no" />
         </div>
     )
 }
