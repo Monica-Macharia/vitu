75a1b56fb182a5578319a90ddcf5fab71653b1bd
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-10-13 16:44:09 +0300

    User login

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,104 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import styles from "./Login.module.css"
-import { loginValidate } from './loginValidate';
-import axios from 'axios';
-import { notify } from './Notifications';
-import { ToastContainer } from 'react-toastify';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Footer, Navbar } from "../components";
 
 const Login = () => {
-    // Edit Title
-    document.title = "Login"
-    // States
-    const [data, setData] = useState({
-        email: "",
-        password: "",
-    });
-    // Errors set
-    const [errors, setErrors] = useState({})
-    // Active Errors on Focuse !
-    const [focus, setFocus] = useState({});
-    // Functions
-    // Change Inputs Value
-    const changeHandelr = (event) => {
-        setData({ ...data, [event.target.name]: [event.target.value] })
-    }
-    // Focus Set
-    const focusHandeler = (event) => {
-        setFocus({ ...focus, [event.target.name]: true })
-    }
-    // validate Data
-    useEffect(() => {
-        setErrors(loginValidate(data))
-    }, [data], [focus])
-    // Login Btn
-    const loginBtn = () => {
-        let flag = false;
-        if (errors.email === true && errors.password === true) {
-            const url = "https://jsonplaceholder.typicode.com/users";
-            axios.get(url)
-                .then(resp => {
-                    if (resp.status === 200) {
-                        for (let i in resp.data) {
-                            // We dont have Password in this fake API so we gotta use zipcode !!
-                            if (data.email === resp.data[i].email && data.password === resp.data[i].address.zipcode) {
-                                flag = true;
-                                setData({
-                                    email: "",
-                                    password: "",
-                                });
-                                setFocus({
-                                    email: false,
-                                    password: false,
-                                });
-                                break;
-                            }
-                        }
-                        if (flag) {
-                            notify("You have been successfully loged in", "succsess")
-                        }
-                        else {
-                            notify("The Email or Password is wrong !", "invalid");
-                            setData({
-                                email: "",
-                                password: "",
-                            });
-                        }
-                    }
-                })
-        } else {
-            setFocus({
-                email: true,
-                password: true,
-            })
-        }
-    }
-    return (
-        // main form
-        <form>
-            {/* Form Titile  */}
-            <h1 className="mainTitle">{document.title}</h1>
-
-            {/* input Items */}
-            <div className={styles.inputItems}>
-                <label>Email</label>
-                <input className={`${styles.inputs} ${errors.email !== true && focus.email === true ? styles.borderSetRed : styles.borderSetDefult}`} type="email" value={data.email} name="email" autoComplete="off" onChange={changeHandelr} onFocus={focusHandeler} />
-                {focus.email && <span>{errors.email}</span>}
-            </div>
-            <div className={styles.inputItems}>
-                <label>Password</label>
-                <input className={`${styles.inputs} ${errors.password !== true && focus.password === true ? styles.borderSetRed : styles.borderSetDefult}`} type="password" value={data.password} name="password" autoComplete="off" onChange={changeHandelr} onFocus={focusHandeler} />
-                {focus.password && <span>{errors.password}</span>}
-            </div>
-
-            {/* Buttons */}
-            <div id={styles.buttons}>
-                <Link to="/Signup" id={styles.signup}>Sign Up</Link>
-                <a href='login' onClick={loginBtn} id={styles.login}>Login</a>
-            </div>
-            <ToastContainer />
-        </form>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">Login</h1>
+        <hr />
+        <div class="row my-4 h-100">
+          <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
+            <form>
+              <div class="my-3">
+                <label for="display-4">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="floatingInput"
+                  placeholder="name@example.com"
+                />
+              </div>
+              <div class="my-3">
+                <label for="floatingPassword display-4">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="floatingPassword"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="my-3">
+                <p>New Here? <Link to="/register" className="text-decoration-underline text-info">Register</Link> </p>
+              </div>
+              <div className="text-center">
+                <button class="my-2 mx-auto btn btn-dark" type="submit" disabled>
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Login;
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,157 +1,58 @@
-// Imports
-import React, { useEffect, useState } from 'react';
-import styles from "./Signup.module.css"
-import { validate } from "./validate"
-import axios from 'axios';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-// Notification
-import { notify } from './Notifications';
+import React from 'react'
+import { Footer, Navbar } from "../components";
 import { Link } from 'react-router-dom';
-
-
 const Signup = () => {
-    // Reset title
-    document.title = "Sign Up";
-    // Setting Data
-    const [data, setData] = useState({
-        name: "",
-        email: "",
-        password: "",
-        confrimPassword: "",
-        checker: false,
-    });
-    // Error Managment
-    const [errors, setErrors] = useState({});
-    // Showing Data on Focus
-    const [focus, setFocus] = useState({});
-
-    // Updating Input Value on Change
-    const updator = (event) => {
-        if (event.target.name === "checker") {
-            setData({ ...data, [event.target.name]: event.target.checked })
-        }
-        else {
-            setData({ ...data, [event.target.name]: [event.target.value] })
-        }
-    }
-
-    // Showing Errors on Focus
-    const focusHandeler = (event) => {
-        setFocus({ ...focus, [event.target.name]: true })
-    }
-    useEffect(() => {
-        setErrors(validate(data));
-    }, [data], [focus]); // ** On change data & focus
-
-
-
-
-    // Sign Up Btn
-    const signUpBtn = () => {
-        // if all accepted **
-        if (errors.name === true && errors.email === true && errors.finalPassword === true && errors.checker === true && errors.password === true && errors.confrimPassword === true) {
-            // Sending Data to API
-            const url = "https://jsonplaceholder.typicode.com/posts";
-            const dataSned = {
-                name: data.name[0],
-                email: data.email[0],
-                password: data.password[0],
-            }
-            axios.post(url, dataSned)
-                .then(resp => {
-                    // Cheking for Status
-                    if (resp.status === 201) {
-                        // nofification Succsess
-                        setData({
-                            name: "",
-                            email: "",
-                            password: "",
-                            confrimPassword: "",
-                            checker: false,
-                        });
-                        setFocus({
-                            name: false,
-                            email: false,
-                            password: false,
-                            confrimPassword: false,
-                            checker: false
-                        });
-                        document.querySelector("input[type=checkbox]").checked = false
-                        notify("You Signed Up Successfully !", "succsess")
-                    }
-                })
-
-
-        } else {
-            setFocus({
-                name: true,
-                email: true,
-                password: true,
-                confrimPassword: true,
-                checker: true
-            })
-            notify("Invalid Item/Items !", "fail")
-
-        }
-    }
-
-    // Buttons
     return (
-        // form
-        <form>
-            {/* titile */}
-            <h1 className="mainTitle">{document.title}</h1>
-            {/* --------Inputs----- */}
-            {/* Name */}
-            <div className={styles.inputItem}>
-                <label>Name</label>
-                <input className={`${styles.gottaBlockSet} ${errors.name !== true && focus.name === true ? styles.borderSetRed : styles.borderSetDefult}`} type="text" value={data.name} name="name" autoComplete="off" onChange={updator} onFocus={focusHandeler} />
-                {focus.name && <span>{errors.name}</span>}
+        <>
+            <Navbar />
+            <div className="container my-3 py-3">
+                <h1 className="text-center">Signup</h1>
+                <hr />
+                <div class="row my-4 h-100">
+                    <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
+                        <form>
+                            <div class="form my-3">
+                                <label for="Name">Full Name</label>
+                                <input
+                                    type="email"
+                                    class="form-control"
+                                    id="Name"
+                                    placeholder="Enter Your Name"
+                                />
+                            </div>
+                            <div class="form my-3">
+                                <label for="Email">Email address</label>
+                                <input
+                                    type="email"
+                                    class="form-control"
+                                    id="Email"
+                                    placeholder="name@example.com"
+                                />
+                            </div>
+                            <div class="form  my-3">
+                                <label for="Password">Password</label>
+                                <input
+                                    type="password"
+                                    class="form-control"
+                                    id="Password"
+                                    placeholder="Password"
+                                />
+                            </div>
+                            <div className="my-3">
+                                <p>Already has an account? <Link to="/login" className="text-decoration-underline text-info">Login</Link> </p>
+                            </div>
+                            <div className="text-center">
+                                <button class="my-2 mx-auto btn btn-dark" type="submit" disabled>
+                                    Signup
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
+            <Footer />
+        </>
+    )
+}
 
-            {/* Email */}
-            <div className={styles.inputItem}>
-                <label>Email</label>
-                <input className={`${styles.gottaBlockSet} ${errors.email !== true && focus.email === true ? styles.borderSetRed : styles.borderSetDefult}`} type="email" value={data.email} name="email" autoComplete="off" onChange={updator} onFocus={focusHandeler} />
-                {focus.email && <span>{errors.email}</span>}
-
-            </div>
-
-            {/* Password  */}
-            <div className={styles.inputItem}>
-                <label>Password</label>
-                <input className={`${styles.gottaBlockSet} ${errors.password !== true && focus.password === true ? styles.borderSetRed : styles.borderSetDefult}`} type="Password" value={data.password} name="password" autoComplete="off" onChange={updator} onFocus={focusHandeler} />
-                {focus.password && <span>{errors.password}</span>}
-
-            </div>
-
-            {/* Confrim  Password */}
-            <div className={styles.inputItem}>
-                <label>Confrim Password</label>
-                <input className={`${styles.gottaBlockSet} ${errors.confrimPassword !== true && focus.confrimPassword === true ? styles.borderSetRed : styles.borderSetDefult}`} type="Password" value={data.confrimPassword} name="confrimPassword" autoComplete="off" onChange={updator} onFocus={focusHandeler} />
-                {focus.confrimPassword && <span>{errors.confrimPassword}</span>}
-
-            </div>
-
-            {/* Accepting Terms */}
-            <div className={styles.inputItem}>
-                <label htmlFor={styles.checker}>I accept terms of <a href="privacy" id={styles.privacy}>privacy policy</a></label>
-                <input id={styles.checker} type="checkbox" value={data.checker} name="checker" autoComplete="off" onChange={updator} onFocus={focusHandeler} />
-                {focus.checker && <span id={styles.termsError}>{errors.checker}</span>}
-
-            </div>
-
-            {/* Buttons */}
-            <div id={styles.buttons}>
-                <Link to="/Login" id={styles.login}>Login</Link>
-                <a href='signup' onClick={signUpBtn} id={styles.signup}>Sign Up</a>
-            </div>
-            <ToastContainer />
-        </form>
-    );
-};
-
-// Export
-export default Signup;
+export default Signup
