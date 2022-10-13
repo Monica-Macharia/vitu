6f88e6c7b7258ce42a29825899d0a76978d4b0ad
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-10-13 19:15:24 +0300

    about section completed

--- client/src/About.js
+++ client/src/About.js
@@ -1,9 +1,9 @@
-// import React from 'react';
+import React from 'react';
 
-// function About(){
-//     return (
-//         <div>
-//             Vitu Vizuri is a one-stop shop for creative designs. Beautifullly 
-//         </div>
-//     )
-// }
+function About(){
+    return (
+        <div>
+            Vitu Vizuri is a rapidly growing one-stop shop for creative designs.It showcases designs from stylists, artists, photographers, and accessory designers from East Africa and the rest of Africa. This is where they can show off their skills, promote their ideas, and network with local and international clients. All of this is intended to demonstrate to the region that fashion is a money-making, creative business while also promoting the “Made in Africa” notion. 
+        </div>
+    )
+}
--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import NavBar from './NavBar.js';
 import {Route, Routes} from 'react-router-dom';
 import Home from './Home.js';
 import Products from './Products.js';
-// import About from './About.js';
+import About from './About.js';
 import Footer from './Footer.js';
 import Login from './Login.js';
 import Signup from './Signup.js';
@@ -22,8 +22,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/Products" element={<Products />}/> 
-         {/* <Route path="/About" element={<About />}/> */}
-        {/* <Route path="/Footer" element={<Footer />}/>  */}
+         <Route path="/About" element={<About />}/>
         <Route path="/Login" element={<Login />}/>
         <Route path="/Signup" element={<Signup />}/> 
         <Route path="/Cart" element={<Cart />}/>
