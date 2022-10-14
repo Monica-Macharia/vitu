589bc9da5e3928a4f67dcdd5d55e0efb87fa69f8
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-14 10:17:36 +0300

    feat: add login + register + logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   resources :users, only:[:index, :show]
   resources :purchases
   resources :products, only:[:index, :show]
+
+  #login
+  post '/login', to: 'sessions#create'
+
+  #signup
+
+  post '/signup', to: 'users#create'
+
+  #logout
+  delete '/logout', to: 'sessions#destroy'
 end
